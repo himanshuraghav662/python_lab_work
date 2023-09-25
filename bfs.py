acd02078d8888a46e5c595f100ebbba11134249e
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2023-09-25 12:04:50 +0530

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,29 @@
+graph = {
+  '1' : ['2','3','4'],
+  '2' : ['2', '5'],
+  '3' : ['6'],
+  '4' : [],
+  '5' : ['6'],
+  '6' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '1')    # function calling   
+                      
